1995a43e60173616a2fad9e91dbd256c3cce0665
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2022-11-22 08:54:24 +0530

    Added first publish

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+// Name of the Cache.
+const CACHE = "playbacksPWACachejatinkambojme";
+
+// Select files for caching.
+let urlsToCache = [
+  "/index.html",
+  "/favicon/favicon.ico",
+  "/favicon/android-chrome-192x192.png",
+  "/favicon/android-chrome-512x512.png",
+  "/favicon/apple-touch-icon.png",
+  "/favicon/favicon-16x16.png",
+  "/favicon/favicon-32x32.png",
+  "/favicon/MJK618-avtar.png",
+  "/assets/icons/icon-48x48.png",
+  "/assets/icons/icon-72x72.png",
+  "/assets/icons/icon-96x96.png",
+  "/assets/icons/icon-128x128.png",
+  "/assets/icons/icon-144x144.png",
+  "/assets/icons/icon-152x152.png",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-384x384.png",
+  "/assets/icons/icon-512x512.png",
+  "/css/styles.css",
+  "/service-worker.js",
+  "js/handler.js",
+];
+
+// Cache all the selected items once application is installed.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      console.log("Caching started.");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Whenever a resource is requested, return if its cached else fetch the resourcefrom server.
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
